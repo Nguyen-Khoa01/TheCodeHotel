fed1ebf6b317e0b005cca3eeae36999975c59566
nguyendangkhoa108 <dangkhoano1.2001@gmail.com>
2023-03-02 16:21:09 +0700

    add file

---
+++ Backend/src/Route/api.ts
@@ -0,0 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import express from 'express';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const router = express.Router();

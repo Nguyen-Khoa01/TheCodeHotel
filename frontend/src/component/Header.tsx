42fc9c1a595f1e372d9d7751223336f82a88b3ca
unknown <dangkhoano1,2001@gmail.com>
2023-04-12 23:23:59 +0700

    update

--- frontend/src/component/Header.tsx
+++ frontend/src/component/Header.tsx
@@ -1,42 +1,40 @@
-import Link from "next/link"
-import { UserIcon, Bars3Icon, EllipsisVerticalIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
+import {
+  UserIcon,
+  Bars3Icon,
+  EllipsisVerticalIcon,
+} from "@heroicons/react/24/outline";
 import { use, useEffect } from "react";
 const mainNav = [
   {
-    display: 'Home',
-    path: "/"
+    display: "Home",
+    path: "/",
   },
   {
-    display: 'About us',
-    path: "/AboutUs"
+    display: "About us",
+    path: "/AboutUs",
   },
   {
-    display: 'News',
-    path: "/news"
+    display: "News",
+    path: "/news",
   },
   {
-    display: 'Contact',
-    path: "/Contact"
+    display: "Contact",
+    path: "/Contact",
   },
-]
+];
 
 export default function Header(props: any) {
-
-
-  const handleout = () => {
-    localStorage.removeItem('TOKEN')
-  }
-
-  let menu
+  let menu;
   if (props.auth) {
     menu = (
-      <div >
+      <div>
         <p className="text-gray-700">{props.user.fullname}</p>
-        <Link href="/Login" onClick={handleout}>
+        <Link href="/Login">
           <span>Logout</span>
         </Link>
       </div>
-    )
+    );
   } else {
     menu = (
       <div>
@@ -47,40 +45,40 @@ export default function Header(props: any) {
           <span>/Register</span>
         </Link>
       </div>
-    )
+    );
   }
 
   // console.log(menu)
 
   return (
-
     <div className="grid grid-cols-6 xl:grid-cols-1 2xl:my-[20px]">
-      <div className="flex col-start-2 col-span-4 justify-between items-center font-sans 
+      <div
+        className="flex col-start-2 col-span-4 justify-between items-center font-sans 
         font-light text-gray-700 h-24 xl:col-start-1 2xl:py-[15px] xl:grid  
-          xl:grid-cols-3 2xl:mx-[35px] lg:max-w-[720px] lg:w-full xl:mx-auto lg:h-16  " >
+          xl:grid-cols-3 2xl:mx-[35px] lg:max-w-[720px] lg:w-full xl:mx-auto lg:h-16  "
+      >
         <div className="2md:block hidden ">
           <Bars3Icon className="h-7 w-7 text-gray-500" />
         </div>
         <div>
           <Link href="/">
-            <img src={`https://htmldesigntemplates.com/html/travelin/images/logo.png`} className="xl:w-[170px]" alt="" />
+            <img
+              src={`https://htmldesigntemplates.com/html/travelin/images/logo.png`}
+              className="xl:w-[170px]"
+              alt=""
+            />
           </Link>
         </div>
         <div className="flex  justify-evenly w-3/6 2xl:w-full 2md:hidden">
-          {
-            mainNav.map((item, index) => (
-              <div
-                key={index}
-              >
-                <Link href={item.path}>
-                  <span> {item.display}</span>
-                </Link>
-              </div>
-            ))
-          }
+          {mainNav.map((item, index) => (
+            <div key={index}>
+              <Link href={item.path}>
+                <span> {item.display}</span>
+              </Link>
+            </div>
+          ))}
         </div>
         <div className="flex items-center uppercase text-sm justify-end 2md:hidden">
-
           <UserIcon className="h-5 w-5 mr-1 text-gray-500 lg:h-4 lg:w-4" />
           {menu}
 
@@ -96,5 +94,5 @@ export default function Header(props: any) {
         </div>
       </div>
     </div>
-  )
+  );
 }

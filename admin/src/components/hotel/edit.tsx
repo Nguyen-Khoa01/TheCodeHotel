847561055d0182980d165e17de967a3a10572f90
unknown <dangkhoano1,2001@gmail.com>
2023-04-19 20:51:08 +0700

    update

--- admin/src/components/hotel/edit.tsx
+++ admin/src/components/hotel/edit.tsx
@@ -3,171 +3,207 @@ import { useTranslate, useApiUrl, BaseKey } from "@refinedev/core";
 import { Edit, getValueFromEvent, useSelect } from "@refinedev/antd";
 
 import {
-    Drawer,
-    DrawerProps,
-    Form,
-    FormProps,
-    Input,
-    InputNumber,
-    Radio,
-    Select,
-    Space,
-    ButtonProps,
-    Avatar,
-    Typography,
-    Upload,
-    Grid,
+  Drawer,
+  DrawerProps,
+  Form,
+  FormProps,
+  Input,
+  InputNumber,
+  Radio,
+  Select,
+  Space,
+  ButtonProps,
+  Avatar,
+  Typography,
+  Upload,
+  Grid,
 } from "antd";
 
 const { Text } = Typography;
 
-import { ICategory } from "interfaces";
-
 type EditProductProps = {
-    drawerProps: DrawerProps;
-    formProps: FormProps;
-    saveButtonProps: ButtonProps;
-    editId?: BaseKey;
+  drawerProps: DrawerProps;
+  formProps: FormProps;
+  saveButtonProps: ButtonProps;
+  editId?: BaseKey;
 };
 export const EditHotel: React.FC<EditProductProps> = ({
-    drawerProps,
-    formProps,
-    saveButtonProps,
-    editId,
+  drawerProps,
+  formProps,
+  saveButtonProps,
+  editId,
 }) => {
-    const t = useTranslate();
-    const apiUrl = useApiUrl();
-    const breakpoint = Grid.useBreakpoint();
+  const t = useTranslate();
+  const apiUrl = useApiUrl();
+  const breakpoint = Grid.useBreakpoint();
 
-    const { selectProps: categorySelectProps } = useSelect<ICategory>({
-        resource: "categories",
-    });
-
-    return (
-        <Drawer
-            {...drawerProps}
-            width={breakpoint.sm ? "500px" : "100%"}
-            zIndex={1001}
-        >
-            <Edit
-                saveButtonProps={saveButtonProps}
-                resource="hotels"
-                recordItemId={editId}
-                contentProps={{
-                    style: {
-                        boxShadow: "none",
-                    },
-                    bodyStyle: {
-                        padding: 0,
-                    },
-                }}
+  return (
+    <Drawer
+      {...drawerProps}
+      width={breakpoint.sm ? "500px" : "100%"}
+      zIndex={1001}
+    >
+      <Edit
+        saveButtonProps={saveButtonProps}
+        resource="hotels"
+        recordItemId={editId}
+        contentProps={{
+          style: {
+            boxShadow: "none",
+          },
+          bodyStyle: {
+            padding: 0,
+          },
+        }}
+      >
+        <Form {...formProps} layout="vertical">
+          <Form.Item label={t("products.fields.images.label")}>
+            <Form.Item
+              name="images"
+              valuePropName="fileList"
+              getValueFromEvent={getValueFromEvent}
+              noStyle
+              rules={[
+                {
+                  required: true,
+                },
+              ]}
             >
-                <Form {...formProps} layout="vertical">
-                    <Form.Item label={t("products.fields.images.label")}>
-                        <Form.Item
-                            name="images"
-                            valuePropName="fileList"
-                            getValueFromEvent={getValueFromEvent}
-                            noStyle
-                            rules={[
-                                {
-                                    required: true,
-                                },
-                            ]}
-                        >
-                            <Upload.Dragger
-                                name="file"
-                                action={`${apiUrl}/media/upload`}
-                                listType="picture"
-                                maxCount={1}
-                                accept=".png"
-                            >
-                                <Space direction="vertical" size={2}>
-                                    <Avatar
-                                        style={{
-                                            width: "100%",
-                                            height: "100%",
-                                            maxWidth: "256px",
-                                        }}
-                                        src="/images/product-default-img.png"
-                                        alt="Store Location"
-                                    />
-                                    <Text
-                                        style={{
-                                            fontWeight: 800,
-                                            fontSize: "16px",
-                                            marginTop: "8px",
-                                        }}
-                                    >
-                                        {t(
-                                            "products.fields.images.description",
-                                        )}
-                                    </Text>
-                                    <Text style={{ fontSize: "12px" }}>
-                                        {t("products.fields.images.validation")}
-                                    </Text>
-                                </Space>
-                            </Upload.Dragger>
-                        </Form.Item>
-                    </Form.Item>
-                    <Form.Item
-                        label={t("products.fields.name")}
-                        name="name"
-                        rules={[
-                            {
-                                required: true,
-                            },
-                        ]}
-                    >
-                        <Input />
-                    </Form.Item>
-                    <Form.Item
-                        label={t("products.fields.description")}
-                        name="desreption"
-                        rules={[
-                            {
-                                required: true,
-                            },
-                        ]}
-                    >
-                        <Input.TextArea rows={6} />
-                    </Form.Item>
-                    <Form.Item
-                        label={t("products.fields.price")}
-                        name="price"
-                        rules={[
-                            {
-                                required: true,
-                            },
-                        ]}
-                    >
-                        <InputNumber
-                            formatter={(value) => `$ ${value}`}
-                            style={{ width: "150px" }}
-                        />
-                    </Form.Item>
-                    <Form.Item
-                        label={t("products.fields.category")}
-                        name={["category", "id"]}
-                        rules={[
-                            {
-                                required: true,
-                            },
-                        ]}
-                    >
-                        <Select {...categorySelectProps} />
-                    </Form.Item>
-                    <Form.Item
-                        label={t("products.fields.isActive")}
-                        name="isActive"
-                    >
-                        <Radio.Group>
-                            <Radio value={true}>{t("status.enable")}</Radio>
-                            <Radio value={false}>{t("status.disable")}</Radio>
-                        </Radio.Group>
-                    </Form.Item>
-                </Form>
-            </Edit>
-        </Drawer>
-    );
+              <Upload.Dragger
+                name="file"
+                action={`${apiUrl}/media/upload`}
+                listType="picture"
+                maxCount={1}
+                accept=".png"
+              >
+                <Space direction="vertical" size={2}>
+                  <Avatar
+                    style={{
+                      width: "100%",
+                      height: "100%",
+                      maxWidth: "256px",
+                    }}
+                    src="/images/product-default-img.png"
+                    alt="Store Location"
+                  />
+                  <Text
+                    style={{
+                      fontWeight: 800,
+                      fontSize: "16px",
+                      marginTop: "8px",
+                    }}
+                  >
+                    {t("products.fields.images.description")}
+                  </Text>
+                  <Text style={{ fontSize: "12px" }}>
+                    {t("products.fields.images.validation")}
+                  </Text>
+                </Space>
+              </Upload.Dragger>
+            </Form.Item>
+          </Form.Item>
+          <Form.Item
+            label={t("products.fields.name")}
+            name="name"
+            rules={[
+              {
+                required: true,
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label={t("products.fields.address")}
+            name="address"
+            rules={[
+              {
+                required: true,
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label={t("products.fields.description")}
+            name="desreption"
+            rules={[
+              {
+                required: true,
+              },
+            ]}
+          >
+            <Input.TextArea rows={6} />
+          </Form.Item>
+          <Form.Item
+            label={t("products.fields.city")}
+            name="city"
+            rules={[
+              {
+                required: true,
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label={t("products.fields.phonenumber")}
+            name="phonenumber"
+            rules={[
+              {
+                required: true,
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label={t("products.fields.starRating")}
+            name="starRating"
+            rules={[
+              {
+                required: true,
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label={t("products.fields.price")}
+            name="price"
+            rules={[
+              {
+                required: true,
+              },
+            ]}
+          >
+            <InputNumber style={{ width: "150px" }} />
+          </Form.Item>
+          <Form.Item
+            label={t("products.fields.category")}
+            name="category"
+            rules={[
+              {
+                required: true,
+              },
+            ]}
+          >
+            <Input></Input>
+          </Form.Item>
+          <Form.Item
+            label={t("products.fields.room")}
+            name="rooms"
+            rules={[
+              {
+                required: true,
+                type: "number",
+              },
+            ]}
+          >
+            <InputNumber style={{ width: "150px" }} />
+          </Form.Item>
+        </Form>
+      </Edit>
+    </Drawer>
+  );
 };
